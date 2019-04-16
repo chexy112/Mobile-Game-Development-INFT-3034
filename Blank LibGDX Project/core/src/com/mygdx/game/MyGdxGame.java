b0021e5210c59cbf488c90ed724e7831be142f68
Xi Chen <chexy112@mymail.unisa.edu.au>
2019-04-17 00:17:09 +0930

    update

--- Blank LibGDX Project/core/src/com/mygdx/game/MyGdxGame.java
+++ Blank LibGDX Project/core/src/com/mygdx/game/MyGdxGame.java
@@ -1,38 +1,33 @@
 package com.mygdx.game;
 
-import com.badlogic.gdx.ApplicationListener;
-import com.badlogic.gdx.Game;
+import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class MyGdxGame extends Game implements ApplicationListener {
-    // The class with the menu
-    public static MenuScreen menuScreen;
-    // The class with the game
-    public static GameScreen gameScreen;
-    @Override
-    public void create() {
-        Gdx.app.log("MyGdxGame: "," create");
-        gameScreen = new GameScreen(this);
-        menuScreen = new MenuScreen(this);
-        Gdx.app.log("MyGdxGame: ","about to change screen to menuScreen");
-// Change screens to the menu
-        setScreen(menuScreen);
-        Gdx.app.log("MyGdxGame: ","changed screen to menuScreen");
-    }
-    @Override
-    public void dispose() {super.dispose();}
-    @Override
-// this method calls the super class render
-// which in turn calls the render of the actual screen being used
-    public void render() {super.render();}
-    @Override
-    public void resize(int width, int height) { super.resize(width, height);}
-    @Override
-    public void pause() {
-        super.pause();
-    }
-    @Override
-    public void resume() {
-        super.resume();
-    }
+public class MyGdxGame extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		batch.draw(img, 0, 0);
+		batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		img.dispose();
+	}
 }
--- Blank LibGDX Project/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ Blank LibGDX Project/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -2,13 +2,11 @@ package com.mygdx.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.mygdx.game.BricksGame;
+import com.mygdx.game.MyGdxGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new BricksGame(), config);
-
-		config.forceExit = true;
+		new LwjglApplication(new MyGdxGame(), config);
 	}
 }
