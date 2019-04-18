18a023bbe4680cce4952997a08b62a33d5dd1dd0
Xi Chen <chexy112@mymail.unisa.edu.au>
2019-04-18 20:37:59 +0930

    adding music

--- Blank LibGDX Project/core/src/com/mygdx/game/BricksGame.java
+++ Blank LibGDX Project/core/src/com/mygdx/game/BricksGame.java
@@ -1,6 +1,8 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.mygdx.game.Screens.MenuScreen;
 
@@ -11,11 +13,16 @@ public class BricksGame extends Game {
     public static final int V_WIDTH = 640;
     public static final int V_HEIGHT = 480;
 
+	public Music bgm;
 
 	@Override
 	public void create () {
 	    batch = new SpriteBatch();
 	    setScreen(new MenuScreen(this));
+		bgm = Gdx.audio.newMusic(Gdx.files.internal("level1.ogg"));
+		bgm.setLooping(true);
+		bgm.setVolume(0.2f);
+		bgm.play();
 
 	}
 
