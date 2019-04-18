e2e60e10a39ee999d58bffa5cc698b015b8740cb
Xi Chen <chexy112@mymail.unisa.edu.au>
2019-04-18 21:32:30 +0930

    assets

---
+++ Blank LibGDX Project/android/assets/map.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="map" tilewidth="160" tileheight="120" tilecount="24" columns="3">
+ <image source="map.png" width="512" height="1024"/>
+</tileset>
